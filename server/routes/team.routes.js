cb4fc6e5ef3d4f0d89753530fe914afb93f3bf17
mtfasmatharik-boop <mtfasmatharik@gmail.com>
2026-07-16 11:35:53 +0530

    feat(routes): register get my team endpoint

--- server/routes/team.routes.js
+++ server/routes/team.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createNewTeam, inviteUser, respondToInvite, fetchMyTeam, fetchAllTeams, joinExistingTeam } from '../controllers/team.controller.js';
+import { createNewTeam, inviteUser, respondToInvite, fetchMyTeam, fetchAllTeams, joinExistingTeam, getMyTeam } from '../controllers/team.controller.js';
 import { verifyToken } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 router.get('/', fetchAllTeams);
 router.get('/mine', verifyToken, fetchMyTeam);
+router.get('/my-team', verifyToken, getMyTeam);
 router.post('/', verifyToken, createNewTeam);
 router.post('/join', verifyToken, joinExistingTeam);
 router.post('/invite', verifyToken, inviteUser);
